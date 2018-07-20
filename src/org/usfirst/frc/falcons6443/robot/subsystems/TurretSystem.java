c2ee8f85579b09ec450344bd5f2b7ad54a8b3eec
LizzyMilford <Lizzym@outofthisworld.net>
2018-07-20 14:28:46 -0700

    -Starting turret branch

---
+++ src/org/usfirst/frc/falcons6443/robot/subsystems/TurretSystem.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.falcons6443.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class TurretSystem extends Subsystem{
+
+    public TurretSystem(){
+
+    }
+
+    @Override
+    public void initDefaultCommand() { }
+}
