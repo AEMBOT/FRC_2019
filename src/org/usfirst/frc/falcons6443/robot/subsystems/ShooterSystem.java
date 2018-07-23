8cc36f31a64bb04915c6e79280821cf68efba8d4
LizzyMilford <Lizzym@outofthisworld.net>
2018-07-23 16:28:00 -0700

    - setting up linear interpolation

--- src/org/usfirst/frc/falcons6443/robot/subsystems/ShooterSystem.java
+++ src/org/usfirst/frc/falcons6443/robot/subsystems/ShooterSystem.java
@@ -5,11 +5,16 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.falcons6443.robot.RobotMap;
 import org.usfirst.frc.falcons6443.robot.hardware.Encoders;
+import org.usfirst.frc.falcons6443.robot.utilities.PID;
 
 public class ShooterSystem extends Subsystem {
 
     private Spark motor;
     private Encoders encoder;
+
+    private double[] chartX = new double[5]; //distance from target
+    private double[] chartY = new double[5]; //speed needed
+    private double[] defaultArray = {0, 10, 20, 30, 40};
 
     private double distance;
 
@@ -18,6 +23,8 @@ public class ShooterSystem extends Subsystem {
         encoder = new Encoders(RobotMap.ShooterEncoderA, RobotMap.ShooterEncoderB);
         encoder.setReverseDirection(false);
         SmartDashboard.putNumber("Distance to Target", 36);
+        SmartDashboard.putNumberArray("Chart X", defaultArray);
+        SmartDashboard.putNumberArray("Chart Y", defaultArray);
     }
 
     @Override
@@ -29,7 +36,7 @@ public class ShooterSystem extends Subsystem {
         encoder.reset();
     }
 
-    public double getEncoder(){
+    public double getEncoderTicks(){
         return encoder.getDistance();
     }
 
@@ -38,8 +45,31 @@ public class ShooterSystem extends Subsystem {
         //get distance to target (inches) from camera
         //(pulling value from ShuffleBoard until vision code done)
         distance = SmartDashboard.getNumber("Distance to Target", 36);
+
         //calculate speed from distance (motion profiling? With linear interpolation?)
+        //linear interpolation
+        chartX = SmartDashboard.getNumberArray("Chart X", defaultArray);
+        chartY = SmartDashboard.getNumberArray("Chart Y", defaultArray);
+        double[] xy = {-1, -1, -1, -1}; //{x1, y1, x2, y2}
+        double desiredSpeed = -1;
+
+        for(int i = 0; i < chartX.length; i++) {
+            if (distance > chartX[i]) {
+                if(i == chartX.length - 1) desiredSpeed = chartY[i];
+                else if (distance < chartX[i + 1]) {
+                    xy[0] = chartX[i];
+                    xy[1] = chartY[i];
+                    xy[2] = chartX[i + 1];
+                    xy[3] = chartY[i + 1];
+                }
+            } else if (distance == chartX[i]) desiredSpeed = chartY[i];
+        }
+
+        if(xy[0] != -1 && desiredSpeed == -1){
+            desiredSpeed = xy[1] + ((xy[3] - xy[1])/(xy[2] - xy[0])) * (distance - xy[0]);
+        }
         //velocity PID to get wheel up to speed (encoder)
-        //feed in ball when at speed (a green light on ShuffleBoard to alert hand feeding)
+
+        //feed in ball when at speed (a green light [boolean] on ShuffleBoard to alert hand feeding)
     }
 }
