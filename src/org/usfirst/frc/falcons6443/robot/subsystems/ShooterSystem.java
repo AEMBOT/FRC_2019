78c076b13f0e04446870aabc05c38c742a9a8d20
LizzyMilford <Lizzym@outofthisworld.net>
2018-07-25 17:18:46 -0700

    -adding pidf to shooter class

--- src/org/usfirst/frc/falcons6443/robot/subsystems/ShooterSystem.java
+++ src/org/usfirst/frc/falcons6443/robot/subsystems/ShooterSystem.java
@@ -5,46 +5,52 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.falcons6443.robot.RobotMap;
 import org.usfirst.frc.falcons6443.robot.hardware.Encoders;
+import org.usfirst.frc.falcons6443.robot.utilities.pid.PIDF;
 
 public class ShooterSystem extends Subsystem {
 
     private Spark motor;
     private Encoders encoder;
+    private PIDF pidf;
 
     private double[] chartX = new double[5]; //distance from target
     private double[] chartY = new double[5]; //speed needed
     private double[] defaultArray = {0, 10, 20, 30, 40};
 
     private double distance;
+    private static final double p = 0;
+    private static final double i = 0;
+    private static final double d = 0;
+    private static final double f = 0;
+    private static final double epsilon = 0;
 
     public ShooterSystem(){
         motor = new Spark(RobotMap.ShooterMotor);
         encoder = new Encoders(RobotMap.ShooterEncoderA, RobotMap.ShooterEncoderB);
+        pidf = new PIDF(p, i, d, f, epsilon);
         encoder.setReverseDirection(false);
+        pidf.setFinishedRange(5); //update value
+        pidf.setMaxOutput(1);
+        pidf.setMinDoneCycles(5);
         SmartDashboard.putNumber("Distance to Target", 36);
-        SmartDashboard.putNumberArray("Chart X", defaultArray);
-        SmartDashboard.putNumberArray("Chart Y", defaultArray);
+        SmartDashboard.putNumberArray("Distance From Target", defaultArray);
+        SmartDashboard.putNumberArray("Speed at Distance", defaultArray);
         SmartDashboard.putBoolean("Load", false);
     }
 
     @Override
     public void initDefaultCommand() { }
 
-    public void setMotor(double power){ motor.set(power); }
-
-    public void resetEncoder() { encoder.reset(); }
-
-    public double getEncoderTicks(){ return encoder.getDistance(); }
-
     public void charge(){
         //get distance to target (inches) from camera
         //(pulling value from ShuffleBoard until vision code done)
         distance = SmartDashboard.getNumber("Distance to Target", 36);
+        //data tables
+        chartX = SmartDashboard.getNumberArray("Distance From Target", defaultArray);
+        chartY = SmartDashboard.getNumberArray("Speed at Distance", defaultArray);
 
         //calculate speed from distance (motion profiling? With linear interpolation?)
         //linear interpolation
-        chartX = SmartDashboard.getNumberArray("Chart X", defaultArray);
-        chartY = SmartDashboard.getNumberArray("Chart Y", defaultArray);
         double[] xy = {-1, -1, -1, -1}; //{x1, y1, x2, y2}
         double desiredSpeed = -1;
 
@@ -63,14 +69,18 @@ public class ShooterSystem extends Subsystem {
         if(xy[0] != -1 && desiredSpeed == -1){
             desiredSpeed = xy[1] + ((xy[3] - xy[1])/(xy[2] - xy[0])) * (distance - xy[0]); //linear interpolation equation
         }
+
+        pidf.setDesiredValue(desiredSpeed);
         //velocity PID to get wheel up to speed (encoder)
+        double power = pidf.calcPID(encoder.getRate());
+        motor.set(power);
 
         //feed in ball when at speed (a green light [boolean] on ShuffleBoard to alert hand feeding)
-
-        /*if(pid.isDone())*/ SmartDashboard.putBoolean("Load", true);
-        /*else*/ SmartDashboard.putBoolean("Load", false);
+        if (pidf.isDone()) SmartDashboard.putBoolean("Load", true);
+        else SmartDashboard.putBoolean("Load", false);
     }
 
+    //turns "Load" light off (mimics motor feeding on a button, logic wise)
     public void shoot(){
         SmartDashboard.putBoolean("Load", false);
     }
